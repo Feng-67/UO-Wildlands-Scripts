51a9233a1344b31c3b7699833fafd0f057bb9e6c
Feng-67 <sjd4826@gmail.com>
2026-09-02 22:27:48 +0800

    Update

--- CUSTOM ITEMS/CitiesOfBritannia.cs
+++ CUSTOM ITEMS/CitiesOfBritannia.cs
@@ -25,36 +25,38 @@ namespace Server.Items
             this.LootType = LootType.Blessed;
 
             // --- TRAMMEL CITIES ---
-            AddCity("(T) Britain", new Point3D(1496, 1628, 10), Map.Trammel);
-            AddCity("(T) Moonglow", new Point3D(4408, 1168, 0), Map.Trammel);
-            AddCity("(T) Trinsic", new Point3D(1845, 2745, 0), Map.Trammel);
-            AddCity("(T) Skara Brae", new Point3D(591, 2139, 0), Map.Trammel);
-            AddCity("(T) Yew", new Point3D(546, 991, 0), Map.Trammel);
-            AddCity("(T) Minoc", new Point3D(2511, 572, 0), Map.Trammel);
-            AddCity("(T) Vesper", new Point3D(2895, 676, 0), Map.Trammel);
-            AddCity("(T) Jhelom", new Point3D(1378, 3817, 0), Map.Trammel);
-            AddCity("(T) Magincia", new Point3D(3734, 2222, 20), Map.Trammel);
+            AddCity("(T) Britain", new Point3D(1431, 1698, 10), Map.Trammel);
+            AddCity("(T) Moonglow", new Point3D(4471, 1178, 0), Map.Trammel);
+            AddCity("(T) Trinsic", new Point3D(1910, 2687, 0), Map.Trammel);
+            AddCity("(T) Skara Brae", new Point3D(590, 2156, 0), Map.Trammel);
+            AddCity("(T) Yew", new Point3D(636, 865, 0), Map.Trammel);
+            AddCity("(T) Minoc", new Point3D(2512, 564, 0), Map.Trammel);
+            AddCity("(T) Vesper", new Point3D(2891, 686, 0), Map.Trammel);
+            AddCity("(T) Jhelom", new Point3D(1332, 3772, 0), Map.Trammel);
+            AddCity("(T) Magincia", new Point3D(3795, 2252, 20), Map.Trammel);
             AddCity("(T) Nujel'm", new Point3D(3770, 1308, 0), Map.Trammel);
-            AddCity("(T) Buccaneer's Den", new Point3D(2713, 2154, 0), Map.Trammel);
-            AddCity("(T) Serpent's Hold", new Point3D(2888, 3474, 15), Map.Trammel);
-            AddCity("(T) New Haven", new Point3D(3503, 2574, 14), Map.Trammel);
+            AddCity("(T) Buccaneer's Den", new Point3D(2723, 2186, 0), Map.Trammel);
+            AddCity("(T) Serpent's Hold", new Point3D(2882, 3469, 15), Map.Trammel);
+            AddCity("(T) New Haven", new Point3D(3498, 2572, 14), Map.Trammel);
 
             // --- FELUCCA CITIES ---
-            AddCity("(F) Britain", new Point3D(1496, 1628, 10), Map.Felucca);
-            AddCity("(F) Moonglow", new Point3D(4408, 1168, 0), Map.Felucca);
-            AddCity("(F) Trinsic", new Point3D(1845, 2745, 0), Map.Felucca);
-            AddCity("(F) Skara Brae", new Point3D(591, 2139, 0), Map.Felucca);
-            AddCity("(F) Yew", new Point3D(546, 991, 0), Map.Felucca);
-            AddCity("(F) Minoc", new Point3D(2511, 572, 0), Map.Felucca);
-            AddCity("(F) Vesper", new Point3D(2895, 676, 0), Map.Felucca);
-            AddCity("(F) Jhelom", new Point3D(1378, 3817, 0), Map.Felucca);
-            AddCity("(F) Magincia", new Point3D(3734, 2222, 20), Map.Felucca);
+            AddCity("(F) Britain", new Point3D(1431, 1698, 10), Map.Felucca);
+            AddCity("(F) Moonglow", new Point3D(4471, 1178, 0), Map.Felucca);
+            AddCity("(F) Trinsic", new Point3D(1910, 2687, 0), Map.Felucca);
+            AddCity("(F) Skara Brae", new Point3D(590, 2156, 0), Map.Felucca);
+            AddCity("(F) Yew", new Point3D(636, 865, 0), Map.Felucca);
+            AddCity("(F) Minoc", new Point3D(2512, 564, 0), Map.Felucca);
+            AddCity("(F) Vesper", new Point3D(2891, 686, 0), Map.Felucca);
+            AddCity("(F) Jhelom", new Point3D(1332, 3772, 0), Map.Felucca);
+            AddCity("(F) Magincia", new Point3D(3795, 2252, 20), Map.Felucca);
             AddCity("(F) Nujel'm", new Point3D(3770, 1308, 0), Map.Felucca);
-            AddCity("(F) Buccaneer's Den", new Point3D(2713, 2154, 0), Map.Felucca);
+            AddCity("(F) Buccaneer's Den", new Point3D(2723, 2186, 0), Map.Felucca);
             AddCity("(F) Serpent's Hold", new Point3D(2888, 3474, 15), Map.Felucca);
 
-            // --- TOKUNO ---
+            // --- OTHER ---
             AddCity("Zento", new Point3D(739, 1255, 30), Map.Tokuno);
+            AddCity("Umbra", new Point3D(1997, 1381, -90), Map.Malas);
+            AddCity("Royal City", new Point3D(810, 3479, -20), Map.TerMur);
         }
 
         private void AddCity(string cityName, Point3D loc, Map map)

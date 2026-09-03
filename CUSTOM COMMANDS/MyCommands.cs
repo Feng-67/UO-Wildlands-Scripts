c3eac2c2482134cd299571a4e6614bc93ac42897
Feng-67 <sjd4826@gmail.com>
2026-09-03 21:07:36 +0800

    Updated commands

--- CUSTOM COMMANDS/MyCommands.cs
+++ CUSTOM COMMANDS/MyCommands.cs
@@ -38,10 +38,9 @@ namespace Server.Gumps
         private readonly List<string> m_AllCommands = new List<string>
 {
     // Admin Commands
-    "props", "dupe", "area remove", "delete", "unhide", "kill", "add", "tele", "skills",
-    "set movable false", "set movable true", "move", "admin", "save", "townhouses", 
-    "addsovs 1000", "addsovs -1000", "AdminBank 2000000000", "AdminBank -2000000000",
-    
+    "props", "dupe", "delete", "unhide", "tele", "skills", "area remove", "kill", "move", "save", "townhouses",
+    "set movable false", "set movable true", "addsovs 1000", "addsovs -1000", "AdminBank 2000000000", "AdminBank -2000000000",
+        
     // Player Commands
     "wallet", "mystats", "chathistory", "chattoggle", "myhouses", "where", "emote", "gethue", "time"
 };
